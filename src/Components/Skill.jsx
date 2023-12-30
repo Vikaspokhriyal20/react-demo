195f5aa564257fdc05b1a201f36c0f077e78e2bc
vikas singh <pokhriyalvikas940@gmail.com>
2023-12-30 18:58:11 +0530

    display flex

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -3,8 +3,77 @@ import React from 'react'
 const Skill = () => {
     return (
         <>
-            <main>
-                <h4>My Skills.</h4>
+            <main className='skills-section'>
+                <div className="skills-content">
+                    <div className="skills-heading">
+                    <h4>Skills & Tools I Used</h4>
+                    </div>
+                    <div className="skills-grid">
+                        <div className="skills-name">
+                            <h4>Html</h4>
+                            <ul>
+                                <li>Semantic Tag</li>
+                                <li>Svg and Map</li>
+                                <li>Image and video</li>
+                                <li>HTML Forms</li>
+                                <li>HTML Multimedia</li>
+                            </ul>
+                        </div>
+                        <div className="skills-name">
+                            <h4>Css</h4>
+                            <ul>
+                                <li>Semantic Tag</li>
+                                <li>Svg and Map</li>
+                                <li>Image and video</li>
+                                <li>HTML Forms</li>
+                                <li>HTML Multimedia</li>
+                            </ul>
+                        </div>
+                        <div className="skills-name">
+                            <h4>Twailwind-Css</h4>
+                            <ul>
+                                <li>Semantic Tag</li>
+                                <li>Svg and Map</li>
+                                <li>Image and video</li>
+                                <li>HTML Forms</li>
+                                <li>HTML Multimedia</li>
+                            </ul>
+                        </div>
+                        <div className="skills-name">
+                            <h4>Javascript</h4>
+                            <ul>
+                                <li>Semantic Tag</li>
+                                <li>Svg and Map</li>
+                                <li>Image and video</li>
+                                <li>HTML Forms</li>
+                                <li>HTML Multimedia</li>
+                            </ul>
+                        </div>
+                        <div className="skills-name">
+                            <h4>React.js</h4>
+                            <ul>
+                                <li>Semantic Tag</li>
+                                <li>Svg and Map</li>
+                                <li>Image and video</li>
+                                <li>HTML Forms</li>
+                                <li>HTML Multimedia</li>
+                            </ul>
+                        </div>
+                        <div className="skills-name">
+                            <h4>Git</h4>
+                            <ul>
+                                <li>Semantic Tag</li>
+                                <li>Svg and Map</li>
+                                <li>Image and video</li>
+                                <li>HTML Forms</li>
+                                <li>HTML Multimedia</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                
+                </div>
+               
             </main>
 
         </>
